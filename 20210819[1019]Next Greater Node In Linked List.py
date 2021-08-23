cce1f36ac40dd9b3b4ecfcf23fd5105f0203fbda
fanglile <fanglile@tctech.onaliyun.com>
2021-08-23 23:48:49 +0800

    [292]Nim Game巴什博弈,主要要想明白，简单题

--- 20210819[1019]Next Greater Node In Linked List.py
+++ 20210819[1019]Next Greater Node In Linked List.py
@@ -37,12 +37,12 @@
 
 # leetcode submit region begin(Prohibit modification and deletion)
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
-
+# todo 单调栈改递归
 class Solution(object):
     def nextLargerNodes(self, head):
         '''
